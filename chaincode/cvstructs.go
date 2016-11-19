bf52754d41fead742ebb755c624a4996914f4631
Dan <tehdanscott@gmail.com>
2016-11-19 15:17:57 +0000

    added cvstructs

---
+++ chaincode/cvstructs.go
@@ -0,0 +1,17 @@
+package main
+
+//Represents qualifications of a student
+type Qualification struct {
+	Insititution string
+	Course       string
+	Year         int
+	Grade        string
+}
+
+//Represents the student object
+type Student struct {
+	ID             int
+	Name           string
+	DateOfBirth    string
+	Qualifications []Qualification
+}
