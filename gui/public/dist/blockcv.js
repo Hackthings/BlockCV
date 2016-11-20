d09d97f5d7b5900fb6646f4b32679809877eeb27
stanacton <stanacton@gmail.com>
2016-11-20 14:43:47 +0000

    Student Search Done

--- gui/public/dist/blockcv.js
+++ gui/public/dist/blockcv.js
@@ -1,14 +1,19 @@
 var app = angular.module("blockcv", ['ngRoute']);
 app.controller("EmployerCtrl", ['$scope', 'BlockCVSvc', function ($scope, BlockCVSvc) {
+    $scope.item = {};
     $scope.search = function search(id) {
         var args = {
             studentId: id,
-            employerName: "Global INC"
+            employerName: "IBM CORP"
         };
-
+        $scope.student = null;
         BlockCVSvc.getStudentEmployer(args)
             .success(function (data) {
-                $scope.student = data.result.message;
+                console.log(JSON.stringify(data));
+                if (!data.result) {
+                    alert(JSON.stringify(data));
+                }
+                $scope.student = JSON.parse(data.result.message);
             })
             .error(function (err) {
                 console.error(err);
